fa46f4134add2da246e2d44f15b98c5933f65b8c
Adrian Cosma <adriancosma_en@yahoo.com>
2016-10-24 23:20:58 +0300

    Added Double generator, Fixed Integer Generator

--- src/main/java/com/jenerator/generators/DefaultObjectGenerator.java
+++ src/main/java/com/jenerator/generators/DefaultObjectGenerator.java
@@ -1,11 +1,19 @@
 package com.jenerator.generators;
 
 import java.lang.reflect.Field;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DefaultObjectGenerator <T> implements ObjectGenerator<T>{
+	public static final int MIN_INT = 1;
+	public static final int MAX_INT = 100;
+	public static final double MIN_DOUBLE = 1.0;
+	public static final double MAX_DOUBLE = 100.0;
+		
 	Logger logger = Logger.getLogger(this.getClass().getName());
+	
 	public T generateTestObject(Class<T> clazz) {
 		T emptyObject;
 		try {
@@ -25,19 +33,41 @@ public class DefaultObjectGenerator <T> implements ObjectGenerator<T>{
 				generateTestStringFromAttributeName(object, field);
 			}
 			
-			if (fieldTypeName.equalsIgnoreCase(Integer.class.getName())){
-				
+			if (isIntegerAttribute(fieldTypeName)){
+				setTestValue(object, field, getRandomInt(MIN_INT, MAX_INT));
+			}
+			
+			if (fieldTypeName.equalsIgnoreCase(double.class.getName())) {
+				setTestValue(object, field, getRandomDouble(MIN_DOUBLE, MAX_DOUBLE));
 			}
 		}
 		
 		return object;
 	}
-
-	private void generateTestStringFromAttributeName(T object, Field field) {
-		setGivenTestValue(object, field, field.getName());
+	
+	private int getRandomInt(int minInt, int maxInt) {
+		return new Random().nextInt(maxInt - minInt) + minInt;
+	}
+	
+	private double getRandomDouble(double minDouble, double maxDouble) {
+		return ThreadLocalRandom.current().nextDouble(MIN_DOUBLE, MAX_DOUBLE);
 	}
 
-	private void setGivenTestValue(T object, Field field, Object givenValue) {
+	private boolean isIntegerAttribute(String fieldTypeName) {
+		return fieldTypeName.equalsIgnoreCase(Integer.class.getName()) || 
+				fieldTypeName.equalsIgnoreCase(int.class.getName());
+	}
+
+	/**
+	 * 
+	 * @param holderObject the object which has the {@code field} attribute.
+	 * @param field - the field which value will be set.
+	 */
+	private void generateTestStringFromAttributeName(T holderObject, Field field) {
+		setTestValue(holderObject, field, field.getName());
+	}
+
+	private void setTestValue(T object, Field field, Object givenValue) {
 		try {
 			field.setAccessible(true);
 			field.set(object, givenValue);
@@ -49,6 +79,10 @@ public class DefaultObjectGenerator <T> implements ObjectGenerator<T>{
 		}
 	}
 
+	/**
+	 * 
+	 * @return true if the {@code fieldTypeName} is String.
+	 */
 	private boolean isStringAttribute(String fieldTypeName) {
 		return fieldTypeName.equalsIgnoreCase(String.class.getName());
 	}
--- src/test/java/com/jenerator/generators/DefaultGeneratorTestCase.java
+++ src/test/java/com/jenerator/generators/DefaultGeneratorTestCase.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import com.jenerator.generators.DefaultObjectGenerator;
 import com.jenerator.generators.ObjectGenerator;
+import com.jenerator.testObjects.DoubleHolder;
 import com.jenerator.testObjects.IntHolder;
 import com.jenerator.testObjects.Person;
 
@@ -14,7 +15,7 @@ public class DefaultGeneratorTestCase {
 	private static final String FIRST_NAME = "firstName";
 
 	@Test
-	public void testStringGenerator() throws Exception {
+	public void testStringGenerator_shouldGenerateValueFromFieldName() throws Exception {
 		ObjectGenerator<Person> personGenerator = new DefaultObjectGenerator<>();
 		Person person = personGenerator.generateTestObject(Person.class);
 		
@@ -23,11 +24,24 @@ public class DefaultGeneratorTestCase {
 	}
 	
 	@Test
-	public void testIntegerGenerator() {
+	public void testIntegerGenerator_shouldGenerateBoundedRandomInt() {
 		ObjectGenerator<IntHolder> integerGenerator = new DefaultObjectGenerator<>();
 		IntHolder intHolder = integerGenerator.generateTestObject(IntHolder.class);
 		
-		assertTrue("Integer generated value should be in the interval [1, 10).", 
-				0 < intHolder.getTestInt() && intHolder.getTestInt() < 10);
-	}	
+		assertTrue("Integer generated value [" + intHolder.getTestInt()
+				+ "] should be in the interval [1, 100).", 
+				DefaultObjectGenerator.MIN_INT <= intHolder.getTestInt()
+				&& intHolder.getTestInt() < DefaultObjectGenerator.MAX_INT);
+	}
+	
+	@Test
+	public void testDoubleGenerator_shouldGenerateBoundedRandomDouble() {
+		ObjectGenerator<DoubleHolder> doubleGenerator = new DefaultObjectGenerator<>();
+		DoubleHolder doubleHolder = doubleGenerator.generateTestObject(DoubleHolder.class);
+		
+		assertTrue("Double generated value [" +  doubleHolder.getTestDouble()
+				+ "] should be in the interval [1, 100)",
+				DefaultObjectGenerator.MIN_DOUBLE <= doubleHolder.getTestDouble() 
+				&& doubleHolder.getTestDouble() < DefaultObjectGenerator.MAX_DOUBLE);
+	}
 }
